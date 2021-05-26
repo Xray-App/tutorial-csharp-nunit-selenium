7b85aea4a1d4607cc88db04f9d0df8a9bc26387d
Sergio Freire <sergio.freire@xpand-it.com>
2021-05-26 10:21:51 +0100

    add script to import to Xray cloud

---
+++ import_results_cloud.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+FILE="TestResults/nunit_webdriver_tests.xml"
+PROJECT=CALC
+TESTPLAN=""
+BROWSER=""
+token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" https://xray.cloud.xpand-it.com/api/v2/authenticate| tr -d '"')
+curl -H "Content-Type: application/xml" -X POST -H "Authorization: Bearer $token"  --data @"$FILE" "https://xray.cloud.xpand-it.com/api/v2/import/execution/nunit?projectKey=$PROJECT&testPlanKey=$TESTPLAN&testEnvironments=$BROWSER"
