32c8dcb54fc00c5d7f73c5513949fc3d5d739882
Cristiano Morais da Cunha <79987166+CMCunha@users.noreply.github.com>
2022-02-01 10:57:10 +0000

    Update import_results_cloud.sh

--- import_results_cloud.sh
+++ import_results_cloud.sh
@@ -4,4 +4,4 @@ PROJECT=CALC
 TESTPLAN=""
 BROWSER=""
 token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" https://xray.cloud.xpand-it.com/api/v2/authenticate| tr -d '"')
-curl -H "Content-Type: application/xml" -X POST -H "Authorization: Bearer $token"  --data @"$FILE" "https://xray.cloud.xpand-it.com/api/v2/import/execution/nunit?projectKey=$PROJECT&testPlanKey=$TESTPLAN&testEnvironments=$BROWSER"
+curl -H "Content-Type: application/xml" -X POST -H "Authorization: Bearer $token"  --data @"$FILE" "https://xray.cloud.getxray.app/api/v2/import/execution/nunit?projectKey=$PROJECT&testPlanKey=$TESTPLAN&testEnvironments=$BROWSER"
