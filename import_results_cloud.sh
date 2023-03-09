c02a84a95598d9d5c85f339cac16165ff656ed35
CMCunha <cristiano.cunha@gmail.com>
2023-03-09 11:38:34 +0000

    Updating Xray endpoint.

--- import_results_cloud.sh
+++ import_results_cloud.sh
@@ -3,5 +3,5 @@ FILE="TestResults/nunit_webdriver_tests.xml"
 PROJECT=CALC
 TESTPLAN=""
 BROWSER=""
-token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" https://xray.cloud.xpand-it.com/api/v2/authenticate| tr -d '"')
+token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" https://xray.cloud.getxray.app/api/v2/authenticate| tr -d '"')
 curl -H "Content-Type: application/xml" -X POST -H "Authorization: Bearer $token"  --data @"$FILE" "https://xray.cloud.getxray.app/api/v2/import/execution/nunit?projectKey=$PROJECT&testPlanKey=$TESTPLAN&testEnvironments=$BROWSER"
